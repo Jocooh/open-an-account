a1da6aaa30e676801ddd0c13adf8ea9128e0698d
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2023-02-08 01:38:16 +0900

    [refactor] 로그인 페이지 스타일 리팩토링

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,5 +1,24 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import {
+  AuthBackground,
+  AuthButton,
+  AuthForm,
+  AuthInput,
+  AuthInputWrapper,
+  AuthLabel,
+  AuthLogo,
+  AuthLogoImg,
+  AuthText,
+  AuthTitle,
+  AuthWrapper,
+  SocialLoginButton,
+  SocialLoginButtonLogo,
+  SocialLoginButtonLogoImg,
+  SocialLoginButtonText,
+  SocialLoginForm,
+  SocialLoginTitle,
+} from './style';
 
 function LoginPage() {
   const socialBtn = [
@@ -58,157 +77,3 @@ function LoginPage() {
 }
 
 export default LoginPage;
-
-// auth background
-const AuthBackground = styled.div`
-  height: 100vh;
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  overflow-y: hidden;
-
-  background-color: #e9e9e9;
-`;
-
-// auth wrapper
-const AuthWrapper = styled.div`
-  width: 420px;
-  /* min-width: 400px; */
-  background-color: white;
-  box-sizing: border-box;
-
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  padding: 1.5rem;
-`;
-
-// auth logo, title
-const AuthLogo = styled.div`
-  width: 250px;
-`;
-const AuthLogoImg = styled.img`
-  width: 100%;
-`;
-const AuthTitle = styled.div`
-  width: 300px;
-  font-size: 1.4rem;
-  font-weight: 600;
-
-  margin-top: 1rem;
-  text-align: center;
-`;
-const AuthText = styled.span`
-  color: #656666;
-  font-size: 0.7rem;
-  font-weight: 600;
-
-  margin: 0.5rem 0 2.2rem;
-`;
-
-// auth form
-const AuthForm = styled.form``;
-const AuthInputWrapper = styled.div`
-  width: 300px;
-
-  display: flex;
-  flex-direction: column;
-`;
-const AuthLabel = styled.label`
-  font-size: 0.8rem;
-  font-weight: 600;
-`;
-const AuthInput = styled.input`
-  height: 1.6rem;
-  font-size: 0.9rem;
-  border: 1px solid #c7cace;
-  border-radius: 0.2rem;
-
-  margin: 0.3rem 0 0.8rem;
-  padding: 0.3rem;
-`;
-const AuthButton = styled.button`
-  width: 300px;
-  height: 2.5rem;
-  color: white;
-  background-color: #1175c7;
-  border: none;
-  font-size: 1rem;
-  font-weight: 600;
-  border-radius: 0.2rem;
-  cursor: pointer;
-
-  margin: 0.7rem 0;
-`;
-
-// social login form
-const SocialLoginTitle = styled.span`
-  font-size: 0.8rem;
-  font-weight: 600;
-
-  display: block;
-  text-align: center;
-  /* color: #656666; */
-  margin: 0.1rem 0 0.6rem;
-  position: relative;
-
-  &::before {
-    width: 130px;
-    height: 1px;
-    background-color: #ddd;
-    position: absolute;
-    left: 2rem;
-    top: 0.6rem;
-    content: '';
-  }
-
-  &::after {
-    width: 130px;
-    height: 1px;
-    background-color: #ddd;
-    position: absolute;
-    right: 2rem;
-    top: 0.6rem;
-    content: '';
-  }
-`;
-const SocialLoginForm = styled.div`
-  width: 300px;
-
-  margin: 0.8rem 0;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
-`;
-
-const SocialLoginButton = styled.div`
-  border: 1px solid #c7cace;
-  border-radius: 0.2rem;
-  width: 48%;
-  height: 2.5rem;
-  cursor: pointer;
-
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  /* justify-content: center; */
-  /* align-items: center; */
-  margin-bottom: 0.4rem;
-`;
-const SocialLoginButtonLogo = styled.div`
-  width: 28px;
-
-  padding: 4px 0 0 10px;
-`;
-const SocialLoginButtonLogoImg = styled.img`
-  width: 100%;
-`;
-const SocialLoginButtonText = styled.div`
-  font-size: 0.9rem;
-  font-weight: 600;
-
-  margin: auto;
-`;
