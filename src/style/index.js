f03e3fa3473a478fce6f9eec3795dc7b2df2979c
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2023-02-10 21:30:09 +0900

    fix. GlobalStyle 폴더 병합

--- src/style/index.js
+++ src/style/index.js
@@ -6,6 +6,22 @@ const GlobalStyle = createGlobalStyle`
   border: 0;
 }
 
+body {
+  max-width: 1440px;
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
+    monospace;
+}
+
+
 html {
   line-height: 1.15;
   -webkit-text-size-adjust: 100%;
