8943afc169c079415cbc2990ddc25ad22d0a11c9
NDH24 <pvc9610@naver.com>
2023-02-10 19:10:33 +0900

    feat: boardpage UI 틀

--- src/pages/BoardPage/BoardPage.jsx
+++ src/pages/BoardPage/BoardPage.jsx
@@ -1,7 +1,38 @@
 import React from 'react';
+import {
+  Container,
+  BestContainer,
+  BestComment,
+  BestInfo,
+  Board,
+  BoardComment,
+  BoardButton,
+} from './style';
 
 function BoardPage() {
-  return <div>BoardPage</div>;
+  return (
+    <>
+      <Container>
+        <BestContainer>
+          <BestInfo>불타는 상품 들어가는곳</BestInfo>
+          <BestComment>인기 글 들어가는곳</BestComment>
+        </BestContainer>
+        <hr />
+        {/* <div>상품 후기</div> */}
+        <Board>
+          <BoardComment>게시글 들어가는 곳입니다. 글을 써주세요</BoardComment>
+          <BoardComment>게시글 들어가는 곳입니다. 글을 써주세요.</BoardComment>
+          <BoardComment>게시글 들어가는 곳입니다. 글을 써주세요.</BoardComment>
+          <BoardComment>게시글 들어가는 곳입니다. 글을 써주세요.</BoardComment>
+          <BoardComment>게시글 들어가는 곳입니다. 글을 써주세요.</BoardComment>
+          <BoardComment>게시글 들어가는 곳입니다. 글을 써주세요.</BoardComment>
+          <BoardComment>게시글 들어가는 곳입니다. 글을 써주세요.</BoardComment>
+          <BoardComment>게시글 들어가는 곳입니다. 글을 써주세요.</BoardComment>
+        </Board>
+        <BoardButton>글쓰기</BoardButton>
+      </Container>
+    </>
+  );
 }
 
 export default BoardPage;
--- src/pages/BoardPage/style.js
+++ src/pages/BoardPage/style.js
@@ -1,1 +1,47 @@
 import styled from 'styled-components';
+
+//전체 페이지
+export const Container = styled.div`
+  margin: 0 120px 0 120px;
+`;
+
+//상단 불타는, 인기 부분
+export const BestContainer = styled.div`
+  display: flex;
+  margin-bottom: 30px;
+`;
+
+//불타는 상품
+export const BestInfo = styled.div`
+  padding: 50px 450px 50px 10px;
+  margin-right: 20px;
+  background-color: gray;
+`;
+
+//인기글
+export const BestComment = styled.div`
+  padding: 50px 450px 50px 10px;
+  background-color: aqua;
+`;
+
+//게시판
+export const Board = styled.div`
+  margin: 40px 100px 0 400px;
+  //background-color: beige;
+`;
+
+//게시글
+export const BoardComment = styled.div`
+  margin-bottom: 10px;
+  text-align: justify;
+`;
+
+//게시글쓰기
+export const BoardButton = styled.button`
+  width: 130px;
+  height: 50px;
+  margin-top: 20px;
+  background-color: red;
+  border: none;
+  float: right;
+`;
