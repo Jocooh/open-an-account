bf4daa9e0fdbf33cebae2527432bc6d3de32c19e
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2023-02-10 20:43:27 +0900

    feat. 로그인/비로그인 상태 간 헤더 및 내비게이트 설정 변경

--- src/components/GlobalComponents/Header/Header.jsx
+++ src/components/GlobalComponents/Header/Header.jsx
@@ -1,34 +1,93 @@
-import React from 'react';
-import { Container, Logo, NavBar, Nav, LoginToggle } from './style';
-import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
+import React from "react";
+import { Container, Logo, NavBar, Nav, LoginToggle } from "./style";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { authService } from "../../../common/firebase";
 
 function Header() {
   const navigate = useNavigate();
-  const [currentUser, setCurrentUser] = useState(false);
+  // 마리나님 코드 임시 주석
+  // const [currentUser, setCurrentUser] = useState(false);
+
+  // 로그인 확인을 위한 세션스토리지 키 확인. 키 존재? => 로그인 되어있음 / 없음 => 로그인 안 되어있음
+  const isLoggedIn = sessionStorage.key(0);
+
+  // // 로그아웃 => 왜 한 번에 안되는지 연구해야함.
+  // const onLogoutClick = () => {
+  //   authService.signOut();
+  //   navigate("/", { replace: true });
+
+  //   alert("로그아웃 되었습니다.");
+  // };
+
+  // 로그아웃
+  const onLogoutClick = () => {
+    authService.signOut().then(() => {
+      alert("로그아웃 되었습니다.");
+      sessionStorage.clear();
+      navigate("/", { replace: true });
+    });
+  };
 
   return (
     <Container>
       <Logo
-        onClick={() => navigate('/')}
-        src={require('../../../assets/star.png')}
-        alt='메인'
+        onClick={() => navigate("/")}
+        src={require("../../../assets/star.png")}
+        alt="메인"
       />
       <NavBar>
-        <Nav onClick={() => navigate('/boardpage')}>커뮤니티</Nav>
-        <Nav onClick={() => navigate('/')}>문의하기</Nav>
+        <Nav onClick={() => navigate("/boardpage")}>커뮤니티</Nav>
+        <Nav onClick={() => navigate("/")}>문의하기</Nav>
         <LoginToggle>
-          {currentUser ? (
+          {!isLoggedIn && (
             <>
-              <Nav onClick={() => navigate('/my')}>마이페이지</Nav>
+              <Nav
+                onClick={() => {
+                  navigate("/signup");
+                }}
+              >
+                회원가입
+              </Nav>
+              <Nav
+                onClick={() => {
+                  navigate("/login");
+                }}
+              >
+                로그인
+              </Nav>
+            </>
+          )}
+          {isLoggedIn && (
+            <>
+              <Nav
+                onClick={() => {
+                  navigate("/mypage");
+                }}
+              >
+                마이페이지
+              </Nav>
+              <Nav
+                onClick={() => {
+                  onLogoutClick();
+                }}
+              >
+                로그아웃
+              </Nav>
+            </>
+          )}
+          {/* 마리나님 코드 임시 주석 */}
+          {/* {currentUser ? (
+            <>
+              <Nav onClick={() => navigate("/my")}>마이페이지</Nav>
               <Nav>로그아웃</Nav>
             </>
           ) : (
             <>
-              <Nav onClick={() => navigate('/signUp')}>회원가입</Nav>
-              <Nav onClick={() => navigate('/login')}>로그인</Nav>
+              <Nav onClick={() => navigate("/signUp")}>회원가입</Nav>
+              <Nav onClick={() => navigate("/login")}>로그인</Nav>
             </>
-          )}
+          )} */}
         </LoginToggle>
       </NavBar>
     </Container>
