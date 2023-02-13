7345c16082fe9744c0e7b7ae99aaec9e5c423661
sangwoo <qtd950@gmail.com>
2023-02-13 21:09:26 +0900

    fix. ServicePageUi수정 및 기능 추가.
    전체적인 UI 수정, 탭 버튼, 예금,적금 버튼 기능 구현.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { current } from "@reduxjs/toolkit";
 import { Fragment } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import Router from "../src/routes/index";
