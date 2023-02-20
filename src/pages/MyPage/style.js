7d04dde6a9133d8edff7e3e05a344e65368161b2
JOCO <111356420+Jocooh@users.noreply.github.com>
2023-02-20 18:51:46 +0900

    feat. mypageUI수정 중

--- src/pages/MyPage/style.js
+++ src/pages/MyPage/style.js
@@ -1,133 +1,122 @@
 import styled from "styled-components";
 
-export const StyledMyContainer = styled.div`
+export const MyPageWrapper = styled.div`
   width: 1440px;
-  margin: 0 auto;
-  @media screen and (max-width: 1930px) {
-    width: 1700px;
-  }
-  @media screen and (max-width: 1600px) {
-    width: 1400px;
-  }
-  @media screen and (max-width: 1024px) {
-    width: 90%;
-  }
-  @media screen and (max-width: 580px) {
-    width: 95%;
-  }
+  height: 1110px;
+  margin: auto;
+  display: flex;
+  gap: 200px;
+  background-color: aliceblue;
 `;
-
-export const StyledMyUpperDiv = styled.div`
-  width: 1600px;
-  height: 220px;
-  background-color: #d9d9d9;
-  margin: 0 auto;
+//여기는 왼쪽 박스 스타일구역
+export const LeftBox = styled.div`
+  width: 437px;
+  /* height: 1170px; */
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  padding: 20px;
+  box-sizing: content-box;
+  background-color: lightcyan;
+`;
+export const UserNicknameDiv = styled.div`
+  height: 90px;
   display: flex;
   align-items: center;
-  padding: 15px;
-  box-sizing: border-box;
-  position: relative;
+`;
 
-  @media screen and (max-width: 1700px) {
-    width: 1500px;
-  }
-  @media screen and (max-width: 1400px) {
-    width: 1200px;
-  }
-  @media screen and (max-width: 1024px) {
-    width: 90%;
-  }
-  @media screen and (max-width: 580px) {
-    width: 95%;
-  } ;
+export const UserContentDiv = styled.div`
+  border-top: 1px solid #aaa;
+  border-bottom: 1px solid #aaa;
+  padding: 10px;
+  height: 172px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+export const ContentDiv = styled.div`
+  display: flex;
+  justify-content: space-between;
+  /* background-color: lightcoral; */
+`;
+export const LogOutBtn = styled.button`
+  width: 84px;
+  height: 32px;
+  border: 1px solid #aaa;
+  border-radius: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 2px;
+  color: #888;
+`;
+
+export const UserAccountDiv = styled.div`
+  display: flex;
+  height: 72px;
+  justify-content: space-between;
+  border-bottom: 1px solid #aaa;
+`;
+
+export const UserHistoryDiv = styled.div`
+  display: flex;
+  background-color: lightcoral;
+  flex-direction: column;
+  gap: 10px;
+`;
+//여기서부터는 오른쪽 박스 스타일 구역
+export const RightBox = styled.div`
+  width: 600px;
+  height: 808px;
+  background-color: lightgreen;
+  padding: 20px;
+  display: flex;
+  box-sizing: content-box;
+`;
+export const RightWrapper = styled.div`
+  width: 508px;
+  height: 706px;
+  margin: auto;
+  background-color: yellow;
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+`;
+
+export const ChangePasswordDiv = styled.div`
+  display: flex;
+  height: 350px;
+  /* background-color: lightgoldenrodyellow; */
+  flex-direction: column;
+  gap: 20px;
+`;
+export const UserInput = styled.input`
+  border-radius: 15px;
+  height: 50px;
+`;
+export const EnrollNumberDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100px;
+  /* background-color: lightseagreen; */
+  gap: 10px;
+`;
+export const ChangeNickNameDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100px;
+  /* background-color: lightseagreen; */
+  gap: 10px;
 `;
 
 export const StyledImage = styled.img`
-  width: 164px;
-  height: 164px;
-  margin-right: 30px;
+  width: 70px;
+  height: 70px;
+  object-fit: contain;
+  border-radius: 50px;
 `;
-
-export const UserInfoContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-export const UserInfor = styled.p`
-  font-size: 27px;
-  font-weight: bold;
-`;
-
-export const UserInfoEdit = styled.p`
-  font-size: 20px;
-  position: absolute;
-  top: 0;
-  right: 20px;
-  cursor: pointer;
-  color: #666;
-  :hover {
-    color: #fff;
-  }
-`;
-
-export const StyledMypageLowerDiv = styled.div`
-  width: 1600px;
-  margin: 0 auto;
-  @media screen and (max-width: 1700px) {
-    width: 1500px;
-  }
-  @media screen and (max-width: 1400px) {
-    width: 1200px;
-  }
-  @media screen and (max-width: 1024px) {
-    width: 90%;
-  }
-  @media screen and (max-width: 580px) {
-    width: 95%;
-  } ;
-`;
-
-export const StyledLowerCategory = styled.div`
-  display: flex;
-  justify-content: space-around;
-`;
-
-export const CategoryList = styled.p`
-  font-size: 22px;
-  font-weight: bold;
-  cursor: pointer;
-  :hover {
-    color: #6a24ff;
-  }
-`;
-
-export const CardContainer = styled.div`
-  width: 1500px;
-  background-color: #dedede;
-  display: flex;
-  flex-wrap: wrap;
-  padding: 30px;
-  box-sizing: border-box;
-  margin-left: 40px;
-  @media screen and (max-width: 1700px) {
-    width: 1500px;
-  }
-  @media screen and (max-width: 1400px) {
-    width: 95%;
-  }
-  @media screen and (max-width: 1024px) {
-    width: 95%;
-  }
-  @media screen and (max-width: 580px) {
-    width: 95%;
-  } ;
-`;
-
-export const StyledCard = styled.div`
-  width: 288px;
-  height: 380px;
-  background-color: white;
-  border-radius: 30px;
-  padding: 20px;
-  margin: 10px 15px;
-  border: 1px solid #6a24ff;
+export const StyledIcons = styled.img`
+  width: 40px;
+  height: 40px;
 `;
