8b7c33be01ef22f29ecc04bcb42d6c31ceeceb23
NDH24 <pvc9610@naver.com>
2023-02-20 18:52:54 +0900

    feat.MainPageUIFixed1.1v

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, Autoplay } from "swiper";
 import "swiper/css/pagination";
@@ -8,19 +9,18 @@ import "swiper/css/navigation";
 import "swiper/css";
 
 import {
-  Wraper,
+  UpWraper,
   BannerImage,
   GreetingBox,
   GreetingTitle,
+  GreetingMent,
   Username,
   Greetingment,
   Greetingcontent,
   ProductFdButton,
-  BoardBestCommentWrap,
-  BoardBestComment,
+  BottomWrap,
   BankList,
   BankListTilte,
-  BottomWrap,
   Sidebar,
   BankAndQna,
   QuestionWrap,
@@ -28,13 +28,37 @@ import {
   FinanceHoneyTip,
   MainPageWraper,
   Bannerimage,
+  DownWraper,
+  TodayTipTitle,
+  TodayTipComment,
+  Tips,
+  Tip,
+  TipTitle,
+  TipComment,
 } from "./style";
 
 const MainPage = () => {
   const navigate = useNavigate();
   return (
     <MainPageWraper>
-      <Wraper>
+      <UpWraper>
+        <GreetingBox>
+          <img src={require("../../assets/mainpage/상단 이미지.png")} />
+          <GreetingTitle>
+            <GreetingMent>팁퍼에서 시작하는</GreetingMent>
+            <Username>00님</Username>
+            <Greetingment>의 금융관리</Greetingment>
+          </GreetingTitle>
+          <Greetingcontent>
+            <div />
+            제 1금융권의 상품만 다루는 안전한 목돈 만들기. 원하는 조건에 맞춰
+            <p />
+            상품을 찾아드릴게요. 한눈에 비교하고, 만기수령액을 확인해봐요.
+          </Greetingcontent>
+          <ProductFdButton onClick={() => navigate("/ServicePage")}>
+            상품 찾기
+          </ProductFdButton>
+        </GreetingBox>
         <Swiper
           modules={[Navigation, Pagination, Autoplay]}
           pagination={{ clickable: true }}
@@ -70,45 +94,15 @@ const MainPage = () => {
           </SilderBanner>
           <SilderBanner>
             <a href="https://mybank.ibk.co.kr/uib/jsp/guest/ntr/ntr70/ntr7010/PNTR701000_i2.jsp?lncd=01&grcd=21&tmcd=121&pdcd=0123&wvcd=***********&banner=ibk_main_mddl_01211210123">
-              <Bannerimage
-                src={require("../../assets/banner/image/banner_02.PNG")}
-              />
+              <BannerImage>
+                <img src={require("../../assets/banner/image/banner_02.PNG")} />
+              </BannerImage>
             </a>
           </SilderBanner>
         </Swiper>
 
-        <GreetingBox>
-          <img
-            src={require("../../assets/star.png")}
-            // src={process.env.PUBLIC_URL + "/public/img/Star 1.png"}
-            alt="로고"
-          />
-          <GreetingTitle>
-            <Username>상우님</Username>
-            <Greetingment>의 첫 금융 ~~을 환영합니다~</Greetingment>
-          </GreetingTitle>
-          <Greetingcontent>
-            <div />
-            모금과 함께하는 저축의 시작!
-            <p /> 주거래 은행, 선호 이율, 기간, 원하는 조건에 맞춰 상품을
-            찾아드릴게요.
-            <p /> 선택은 여러분의 자유예요.
-          </Greetingcontent>
-          <ProductFdButton
-            onClick={() => {
-              navigate("/servicepage");
-            }}
-          >
-            상품 찾아보기
-          </ProductFdButton>
-        </GreetingBox>
         <BottomWrap>
-          <BoardBestCommentWrap>
-            <BoardBestComment>
-              <div>게시판 인기글</div>
-            </BoardBestComment>
-          </BoardBestCommentWrap>
-          <BankAndQna>
+          {/* <BankAndQna>
             <BankList>
               <BankListTilte>은행사이트 바로가기</BankListTilte>
               <BankBannerWrap>
@@ -155,16 +149,66 @@ const MainPage = () => {
             <QuestionWrap>
               <QuestionTitle>QnA 자주묻는 질문</QuestionTitle>
             </QuestionWrap>
-          </BankAndQna>
+          </BankAndQna> */}
         </BottomWrap>
-        <FinanceHoneyTip>
-          <div>금융 꿀팁</div>
-        </FinanceHoneyTip>
+      </UpWraper>
+      <DownWraper>
+        <TodayTipTitle>오늘의 금융 팁</TodayTipTitle>
+        <TodayTipComment>
+          금융지식 또는 금융이슈를 전달해드려요. 차곡차곡 쌓은 지식을 통해
+          선택의 폭을 넓혀봐요.
+        </TodayTipComment>
+        <Tips>
+          <Tip>
+            <TipTitle>정기예금</TipTitle>
+            <TipComment>
+              <div>정해진 기간 동안 일정액을 매월 적립하고</div>
+              <div>만기일에 약정금액을 지급받는 상품이에요.</div>
+              <div>매달 혹은 일정기간, 꾸준히 저축하고 싶은</div>
+              <div>친구들은 정기적금을 찾아보아요.</div>
+            </TipComment>
 
-        <Sidebar>
-          <button>1:1 전문상담</button>
-        </Sidebar>
-      </Wraper>
+            <img src={require("../../assets/mainpage/정기예금.png")} />
+          </Tip>
+          <Tip>
+            <TipTitle>정기적금</TipTitle>
+            <img src={require("../../assets/mainpage/정기적금.png")} />
+          </Tip>
+        </Tips>
+        <Tips>
+          <Tip>
+            <TipTitle
+              style={{
+                marginBottom: "25px",
+              }}
+            >
+              이자를 계산하는 법1
+            </TipTitle>
+            <img
+              src={require("../../assets/mainpage/단리.png")}
+              style={{ marginTop: "140px" }}
+            />
+            <TipTitle>단리</TipTitle>
+          </Tip>
+          <Tip>
+            <TipTitle
+              style={{
+                marginBottom: "25px",
+              }}
+            >
+              이자를 계산하는 법2
+            </TipTitle>
+            <TipTitle>복리</TipTitle>
+            <div>
+              {/* <img src={require("../../assets/mainpage/복리화살표.png")} /> */}
+              <img
+                src={require("../../assets/mainpage/복리.png")}
+                style={{ marginTop: "110px" }}
+              />
+            </div>
+          </Tip>
+        </Tips>
+      </DownWraper>
     </MainPageWraper>
   );
 };
