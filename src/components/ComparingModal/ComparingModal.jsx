f98909b8fef2c4886e12843cd73b1d45b7d06aae
MarinaNam <namma123@naver.com>
2023-02-20 18:20:11 +0900

    feat. UI 수정

--- src/components/ComparingModal/ComparingModal.jsx
+++ src/components/ComparingModal/ComparingModal.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import {
   CloseButton,
   FirstGuide,
+  Highlight,
   Input,
   Message,
   MessageWrapper,
@@ -46,16 +47,12 @@ const ComparingModal = ({
         <ModalContents>
           <TitleWrapper>
             <Title>상품 비교</Title>
-            <SubTitle>
-              만기 수령액은 비교 상품들 모두 가입이 가능한 12개월로
-              산정되었습니다.
-            </SubTitle>
           </TitleWrapper>
           <MessageWrapper>
             {selectedProduct[0].category === "예금 기본 정보" ? (
               <>
                 <Message>
-                  12개월 동안
+                  <Highlight>12개월 </Highlight>동안
                   <Input
                     maxLength={11}
                     value={inputValue}
@@ -65,10 +62,10 @@ const ComparingModal = ({
                     inputLength={inputValue.length}
                   />
                   원을 예치하면
-                  <FirstGuide>
-                    *금액은 10억원 미만으로 입력할 수 있습니다.
-                  </FirstGuide>
                 </Message>
+                <FirstGuide>
+                  * 금액은 10억 미만으로 입력할 수 있습니다.
+                </FirstGuide>
                 <Products>
                   <Product
                     inputValue={inputValue}
@@ -105,7 +102,7 @@ const ComparingModal = ({
                 <Message>
                   12개월 동안
                   <Input
-                    maxLength={12}
+                    maxLength={11}
                     value={inputValue}
                     onChange={(e) => setInputValue(e.target.value)}
                     thousandSeparator=","
@@ -114,7 +111,7 @@ const ComparingModal = ({
                   />
                   원 씩 적립하면
                   <FirstGuide>
-                    *금액은 최대 10억원까지 입력할 수 있습니다.
+                    * 금액은 최대 10억 미만으로 입력할 수 있습니다.
                   </FirstGuide>
                 </Message>
                 <Products>
@@ -152,8 +149,12 @@ const ComparingModal = ({
           </MessageWrapper>
 
           <SecondGuide>
-            *만기수령액은 이자소득세를 제외한 (일반과세 기준 이자금액의
+            만기수령액은 이자소득세를 제외한 (일반과세 기준 이자금액의
             15.4%)금액 입니다. 계산 결과는 최고금리가 적용되었습니다.
+          </SecondGuide>
+          <SecondGuide>
+            만기 수령액은 비교 상품들 모두 가입이 가능한 12개월로
+            산정되었습니다.
           </SecondGuide>
         </ModalContents>
       </ModalContainer>
