3d0b8a9023b5ea7c9e511fea981bc877416c4888
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2023-02-25 23:47:13 +0900

    fix. 글로벌 스타일 기존 변경 및 sign up 스타일 변경.

--- src/components/Auth/style.js
+++ src/components/Auth/style.js
@@ -3,7 +3,8 @@ import styled from "styled-components";
 // auth background
 export const AuthBackground = styled.div`
   /* width: 100vw; */
-  height: calc(100vh - 11.6rem);
+  /* height: calc(100vh - 145px); */ // 푸터까지
+  height: calc(100vh - 78px);
 
   display: flex;
   justify-content: center;
