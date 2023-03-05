fd1d7ca906944b9923b7dae3bf7ff7ff109993dd
MarinaNam <namma123@naver.com>
2023-03-05 16:26:20 +0900

    feat. 수정창 앞으로 띄우기

--- src/components/EditPostingModal/style.js
+++ src/components/EditPostingModal/style.js
@@ -1,11 +1,8 @@
 import styled from "styled-components";
 
 export const ModalBackground = styled.div`
-  /* position: fixed;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  right: 0; */
+  position: fixed;
+
   /* background: rgba(67, 79, 101, 0.7); */
 `;
 
