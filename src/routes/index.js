f561440f4a6c36dcbf21c4b4f29d9659b731a4b3
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2023-03-07 13:41:46 +0900

    fix. 랜딩페이지 내 컨텐츠 내비게이트 이동 시 스크롤 초기화

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,10 +10,12 @@ import ServicePage from "../pages/ServicePage/ServicePage";
 import InsertData from "../data/InsertData";
 import Footer from "../../src/components/GlobalComponents/Footer/Footer.jsx";
 import Header from "../../src/components/GlobalComponents/Header/Header.jsx";
+import ScrollToTop from "../components/GlobalComponents/ScrollToTop";
 
 const Router = () => {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Header />
       <Routes>
         <Route path="/" element={<LandingPage />} />
