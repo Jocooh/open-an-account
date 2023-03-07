3a1d1ca3d7751a42cb66a52a879674582fe7a358
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2023-03-07 14:39:39 +0900

    feat. not found 페이지 추가

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,6 +11,7 @@ import InsertData from "../data/InsertData";
 import Footer from "../../src/components/GlobalComponents/Footer/Footer.jsx";
 import Header from "../../src/components/GlobalComponents/Header/Header.jsx";
 import ScrollToTop from "../components/GlobalComponents/ScrollToTop";
+import NoutFountPage from "../pages/NotFountPage";
 
 const Router = () => {
   return (
@@ -28,6 +29,7 @@ const Router = () => {
         <Route path="/detail" element={<DetailPage />} />
         {/* admin */}
         <Route path="/4jojoa" element={<InsertData />} />
+        <Route path="*" element={<NoutFountPage />} />
       </Routes>
       <Footer />
     </BrowserRouter>
